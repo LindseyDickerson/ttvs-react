a45b5f9161a2aea3359b3aa9b1f165479c2f33a6
Lindsey Dickerson <lindseydickerson2@gmail.com>
2020-10-26 14:28:58 -0400

    remove services link

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -18,7 +18,7 @@ import {Route, Link, Switch} from 'react-router-dom';
 
 import Home from './Home';
 import About from './About';
-import Services from './Services';
+// import Services from './Services';
 // import Portfolio from './Portfolio';
 import Logo from '../Assets/WatercolorLogoWText.png';
 
@@ -36,7 +36,7 @@ function NavbarComponent(props) {
                {/* <li><img src={Logo} alt="logo" className="logo"></img></li> */}
                <li><Link to="/">Home</Link></li>
                <li><Link to="/about">About</Link></li>
-               <li><Link to="/services">Services</Link></li>
+               {/* <li><Link to="/services">Services</Link></li> */}
                {/* <li><Link to="/portfolio">Portfolio</Link></li> */}
                <li><a href="https://www.honeybook.com/widget/tenacious_tulip_virtual_services_164228/cf_id/5f74e9d6717a6a709e43fb84" target="_blank" rel="noopener noreferrer">Contact</a></li>
                <li><a href="https://ttvs.hbportal.co/app/" target="_blank" rel="noopener noreferrer">Customer Portal</a></li>
@@ -47,7 +47,7 @@ function NavbarComponent(props) {
            <Switch>
                <Route exact path="/"><Home /></Route>
                <Route exact path="/about"><About/></Route>
-               <Route exact path="/services"><Services/></Route>
+               {/* <Route exact path="/services"><Services/></Route> */}
                
            </Switch>
        </div>
--- src/Components/Services.js
+++ src/Components/Services.js
@@ -3,12 +3,7 @@ import React from "react";
 const Home = () => (
     <div className="container">
             <div className="row">
-                <div className="col-sm" id="blurb1">
-                    Social media is rapidly changing. Keeping up with all the changes, hashtags, and replying to messages & comments is literally a full time job now! Don't let keeping up with social media get in the way of your business goals and priorities.
-                </div>
-                <div className="col-sm" id="blurb2">
-                    Your business can thrive with social media management. It allows more time for you to focus on the core aspects of your business that need the most attention. Your online presence is a great way to reflect your business and help you achieve the growth you need to be successful.
-                </div>
+                
             </div>
         </div>
 );
