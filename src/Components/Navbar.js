fd8ebcb001758f43218e38f096d2daf9d2a095c1
Lindsey Dickerson <lindseydickerson2@gmail.com>
2021-03-12 16:18:15 -0500

    cleaning up text

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import {BrowserRouter as Router} from 'react-router-dom';
 import {Route, Link, Switch} from 'react-router-dom';
 // import {
